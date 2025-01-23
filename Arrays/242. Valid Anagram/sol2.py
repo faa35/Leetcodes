6e94051d10c04f3c65187baab3e39fc029e22e55
Fardin <faa35@sfu.ca>
2025-01-22 19:34:41 -0800

    sol 2 for 242

--- Arrays/242. Valid Anagram/sol2.py
+++ Arrays/242. Valid Anagram/sol2.py
@@ -1,36 +1,3 @@
-```python
-
-class Solution(object):
-    def isAnagram(self, s, t):
-        """
-        :type s: str
-        :type t: str
-        :rtype: bool
-        """
-        if len(s) != len(t):
-            return False
-        
-
-
-        if sorted(s) == sorted(t):
-            return True
-        else:
-            return False
-        
-        #or we can use the below code
-        #return sorted(s) == sorted(t)
-        
-s = "anagram"
-t = "nagaram"
-sol = Solution()
-print(sol.isAnagram(s, t))
-
-
-```
-
-
-```python
-
 class Solution(object):
     def isAnagram(self, s, t):
         """
@@ -66,6 +33,3 @@ s = "anagram"
 t = "nagaram"
 sol = Solution()
 print(sol.isAnagram(s, t))
-```
-
-https://neetcode.io/solutions/valid-anagram 
